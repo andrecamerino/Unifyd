77a421534f90788ce9fe4d84fdd45df5f2729657
andrecamerino <camerinoandre@gmail.com>
2025-04-23 05:08:24 +1200

    create class post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import ClassPost from "@/Components/ClassPost";
+import postsData from "@/example_data/ClassPosts.json";
 
 export default function Home() {
-  return <>
-  <h1 className="text">Home</h1>
-  </>;
+  return <div className="pt-6 pl-6 pr-6 flex flex-col no-scrollbar">
+  {postsData.map((post) => (
+    <ClassPost key={post.postId} post={post} />
+  ))}
+</div>
 }
