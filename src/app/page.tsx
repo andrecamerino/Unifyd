796377ff8a02a4149669a8aba0a6b832ba9d614d
andrecamerino <camerinoandre@gmail.com>
2025-04-24 03:50:59 +1200

    add club post to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import ClassPost from "@/Components/ClassPost";
 import postsData from "@/example_data/ClassPosts.json";
+import clubPostsData from "@/example_data/ClubPosts.json";
+import ClubPost from "@/Components/Clubpost";
 
 export default function Home() {
   return <div className="pt-6 pl-6 pr-6 flex flex-col no-scrollbar">
+    {clubPostsData.map((post) => (
+    <ClubPost key={post.postId} post={post} />
+  ))}
   {postsData.map((post) => (
     <ClassPost key={post.postId} post={post} />
   ))}
